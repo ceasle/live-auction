ded61632afd76166fbe9e7c5ae24f7fce7b6d1f7
ceasle <sujithrockzzzz36@gmail.com>
2022-08-03 12:30:25 +0530

    api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./styles.css";
+import { getdata } from "./service";
+import axios from "axios";
+
+function a():any {
+  axios.get('https://infinite-escarpment-04245.herokuapp.com/')
+  .then((Response) =>{
+    console.log('sujith',Response.data);
+    return Response.data;
+  })
+}
 
 export default function App() {
+  
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <div className="App">
+      <h1>{a()}</h1>
       <h2>Start editing to see some magic happen!</h2>
-    </div>
+      </div>
   );
 }
