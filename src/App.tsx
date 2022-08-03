82ae9be022a94a1986ad77b8ed821c7293362e76
MdNaseerHussain <mdnajju013@gmail.com>
2022-08-03 12:59:48 +0530

    Api call in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import "./styles.css";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-function a():any {
-  axios.get('https://infinite-escarpment-04245.herokuapp.com/')
-  .then((Response) =>{
-    console.log('sujith',Response.data);
-    return Response.data;
-  })
-}
+const axiosConfig = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+};
 
 export default function App() {
-  
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://infinite-escarpment-04245.herokuapp.com/", axiosConfig)
+      .then((Response) => {
+        console.log("sujith", Response.data);
+        setData(Response.data);
+      });
+  }, []);
+
   return (
-      <div className="App">
-      <h1>{a()}</h1>
+    <div className="App">
+      <h1>{data}</h1>
       <h2>Start editing to see some magic happen!</h2>
-      </div>
+    </div>
   );
 }
