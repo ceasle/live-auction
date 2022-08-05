9560b08e146beb024128d58570d06e185c3d453e
ceasle <sujithrockzzzz36@gmail.com>
2022-08-05 10:49:17 +0530

    added deafult page

---
+++ src/components/Default/Default.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+// import { DEFAULT_PAGE_URL } from "../../constants/apiUrls";
+// import { Loading } from "../shared/Loading/Loading";
+// import axios from "axios";
+
+import "./Default.css";
+
+export const Default = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+};
