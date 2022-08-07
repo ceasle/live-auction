fe830e9b6649feba23026a4c80ca2fad5e16194d
MdNaseerHussain <mdnajju013@gmail.com>
2022-08-07 22:03:43 +0530

    Loading component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./components/Home/Home";
 import { CreateAuction } from "./components/CreateAuction/CreateAuction";
 import { Auction } from "./components/Auction/Auction";
 import { Default } from "./components/Default/Default";
+import { Loading } from "./components/shared/Loading/Loading";
 
 export default function App() {
   return (
@@ -13,6 +14,9 @@ export default function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/create" element={<CreateAuction />} />
         <Route path="/auction" element={<Auction />} />
+        {/* Add Testing UI component to this route 
+            Remove the route before prod */}
+        <Route path="/test" element={<Loading />} />
       </Routes>
     </Router>
   );
--- src/components/Default/Default.tsx
+++ src/components/Default/Default.tsx
@@ -1,16 +1,18 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
-// import { DEFAULT_PAGE_URL } from "../../constants/apiUrls";
-// import { Loading } from "../shared/Loading/Loading";
-// import axios from "axios";
-
-import "./Default.css";
+import { DEFAULT_PAGE_URL } from "../../constants/apiUrls";
+import { Loading } from "../shared/Loading/Loading";
 
 export const Default = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+  const [data, setData] = useState<String>("");
+  const [isLoading, setLoadingData] = useState<boolean>(true);
+
+  useEffect(() => {
+    axios.get(DEFAULT_PAGE_URL).then((response) => {
+      setData(response.data);
+      setLoadingData(false);
+    });
+  }, []);
+
+  return <>{isLoading ? <Loading /> : <div>{data}</div>}</>;
 };
