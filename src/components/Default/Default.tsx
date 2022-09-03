ad80a9b1c807655b888d828315572e847b3452f6
ceasle <sujithrockzzzz36@gmail.com>
2022-09-03 11:35:28 +0530

    default page added

--- src/components/Default/Default.tsx
+++ src/components/Default/Default.tsx
@@ -1,18 +1,102 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { DEFAULT_PAGE_URL } from "../../constants/apiUrls";
+import { DEFAULT_PAGE_URL, JOIN_PAGE_URL } from "../../constants/apiUrls";
 import { Loading } from "../shared/Loading/Loading";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+import "./Default.css";
 
 export const Default = () => {
-  const [data, setData] = useState<String>("");
-  const [isLoading, setLoadingData] = useState<boolean>(true);
+  const [isLoading, setLoadingData] = useState<boolean>(false);
+  // useEffect(() => {
+  //   axios.get(DEFAULT_PAGE_URL).then((response) => {
+  //     setLoadingData(false);
+  //   });
+  // }, []);
 
-  useEffect(() => {
-    axios.get(DEFAULT_PAGE_URL).then((response) => {
-      setData(response.data);
-      setLoadingData(false);
-    });
-  }, []);
+  const Default_page = () => {
+    return (
+      <div>
+        {ButtonAppBar()}
+        <div className="home">{FormJoinCode()}</div>
+      </div>
+    );
+  };
 
-  return <>{isLoading ? <Loading /> : <div>{data}</div>}</>;
+  function ButtonAppBar() {
+    const navigate = useNavigate();
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Live Auction
+            </Typography>
+            <Button
+              color="inherit"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
+
+  function FormJoinCode() {
+    return (
+      <Formik
+        initialValues={{
+          entry_code: "",
+        }}
+        validationSchema={Yup.object({
+          // TODO: change size of code from 3 to >6
+          entry_code: Yup.string()
+            .max(3, "Enter valid code")
+            .min(3, "Enter valid code")
+            .required("Required"),
+        })}
+        onSubmit={(values) => {
+          // setLoadingData(true);
+          // setData(values);
+          axios
+            .post(JOIN_PAGE_URL, {
+              Entry_code: values.entry_code,
+            })
+            .then((response) => {
+              // TODO: complete here
+              alert("success");
+              console.log(response);
+              // setLoadingData(false);
+            })
+            .catch((error) => {
+              // TODO: complete here
+              alert("failure");
+              console.log(error);
+              // setLoadingData(false);
+            });
+        }}
+        render={({ values }) => (
+          <Form className="form">
+            <Field name="entry_code" type="text" size="medium" />
+            <ErrorMessage name="entry_code" />
+            <button type="submit">Join Auction</button>
+          </Form>
+        )}
+      />
+    );
+  }
+
+  return <>{isLoading ? <Loading /> : <div>{Default_page()}</div>}</>;
 };
--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -8,4 +8,5 @@ export const DEFAULT_PAGE_URL = BASE_PAGE_URL + "/";
 export const HOME_PAGE_URL = BASE_PAGE_URL + "/home";
 export const CREATE_PAGE_URL = BASE_PAGE_URL + "/create";
 export const AUCTION_PAGE_URL = BASE_PAGE_URL + "/auction";
+export const JOIN_PAGE_URL = BASE_PAGE_URL + "/join";
 export const LOGIN_PAGE_URL = BASE_PAGE_URL + "/login";
